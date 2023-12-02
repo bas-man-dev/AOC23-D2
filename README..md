8e4f3a6977df96feba48337a05700e68746d5b0d
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-12-02 23:11:51 +0000

    first part finished

---
+++ README..md
@@ -0,0 +1,5 @@
+# Advent of Code Day 2
+
+## Part 1
+
+- tidied up the data table as there was a spare gap at the end (learnt from yesterday's data set)
--- cubes.py
+++ cubes.py
@@ -1,1 +1,32 @@
-print('Hello World!')
+import re
+
+RED = 12
+GREEN = 13
+BLUE = 14
+
+result_1 = 0
+
+with open("data.txt") as my_file:
+    for line in my_file:
+        line =  line.strip()
+        split_data = re.split('[:;,]', line)
+
+        for idx, item in enumerate(split_data):
+            game_cubes = item.split()
+
+            if idx == 0:
+                ID = int(game_cubes[1])
+
+            if game_cubes[1] == 'red':
+                if int(game_cubes[0]) > RED:
+                    ID = 0
+            elif game_cubes[1] == 'green':
+                if int(game_cubes[0]) > GREEN:
+                    ID = 0
+            elif game_cubes[1] == 'blue':
+                if int(game_cubes[0]) > BLUE:
+                    ID = 0
+
+        result_1 += ID
+
+    print(f"Result Part 1: {result_1}")
